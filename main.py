96057206e88c4bb7b5ec2d5ca16278668cffc906
eltonmelo189-hub <eltonmelo189@gmail.com>
2026-03-25 09:37:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamllt as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -7,16 +7,16 @@ try:
     dados = pd.read_csv('data/football_data.csv')
     st.sidebar.header("Menu")
     if not dados.empty:
-      c = st.sidebar.selectbox("Analise:", options=dados.columns)
-      L = st.sidebar.selectbox("Liga:", ["Série A Brasil", "Premier League"])
-       df = dados[dados['liga'] == L] if 'liga' in dados.columns else dados
-      st.title(f"Predator: {L}")
-       if df.empty:
-           st.warning("Sem jogos.")
-      else:
-           st.write(df)
-          fig, ax = plt.subplots()
-           l sns.histplot(data=df, x=c, kde=True, ax=ax)
-           st.pyplot(fig)
+        c = st.sidebar.selectbox("Analise:", options=dados.columns)
+        L = st.sidebar.selectbox("Liga:", ["Série A Brasil", "Premier League"])
+        df = dados[dados['liga'] == L] if 'liga' in dados.columns else dados
+        st.title(f"Predator: {L}")
+        if df.empty:
+            st.warning("Sem jogos.")
+        else:
+            st.write(df)
+            fig, ax = plt.subplots()
+            sns.histplot(data=df, x=c, kde=True, ax=ax)
+            st.pyplot(fig)
 except Exception as e:
-  st.error(f"Erro: {e}")
+    st.error(f"Erro: {e}")
